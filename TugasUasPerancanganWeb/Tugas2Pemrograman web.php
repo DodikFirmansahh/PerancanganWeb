07ce2b051e6def1af7e9d7864d91cb7125000117
DodikFirmansahh <134833762+DodikFirmansahh@users.noreply.github.com>
2023-07-05 14:07:54 +0700

    MembuatWebMenaraCentratama

---
+++ TugasUasPerancanganWeb/Tugas2Pemrograman web.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>Halaman HTML</h1>
+
+    <?php
+    
+    $nilai1 = 10;
+    $nilai2 = 7;
+    $nilai3 = 2 * $nilai1 + 8 * $nilai2;
+
+    echo "Total Nilai = ", $nilai_3;
+    echo "<br>";
+    $jumlah = $nilai1 + $nilai2;
+    echo "Hasil dari $nilai1 + $nilai2 adalah : $jumlah"; echo "<br><br>";
+    echo "\"Nama : Dodik Firmansah\" <br>";
+    echo "NIM : 121103018";
+
+    ?>
+    </body>
+    </html>
